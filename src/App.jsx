591de759c3e85433b0a84ff4a74059952c70f8f5
Agustín Seoane <agustinseoane96@gmail.com>
2023-08-05 14:10:56 -0300

    title-changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,36 @@
-import quotes from './db simulation/fortunePhrases.json' 
-import { useState } from 'react'
-import QuoteBox from './components/QuoteBox/QuoteBox'
-import { getRandom } from './utils/random' //function for get random index
-import o from './utils/o.png'
-import './App.css'
+import quotes from "./db simulation/fortunePhrases.json";
+import { useState } from "react";
+import QuoteBox from "./components/QuoteBox/QuoteBox";
+import { getRandom } from "./utils/random"; //function for get random index
+import o from "./utils/o.png";
+import "./App.css";
 
-const imgBgArray = ['bg', 'bg2', 'bg3', 'bg4']
+const imgBgArray = ["bg", "bg2", "bg3", "bg4"];
 
 function App() {
-  const [ quote, setQuote] = useState(getRandom(quotes))
-  const [ currentImg, setCurrentImg ] = useState(getRandom(imgBgArray))
+  const [quote, setQuote] = useState(getRandom(quotes));
+  const [currentImg, setCurrentImg] = useState(getRandom(imgBgArray));
 
   const handleRandomPhrase = () => {
     setQuote(getRandom(quotes));
     setCurrentImg(getRandom(imgBgArray));
-  }
-
+  };
 
   return (
-      <main className={`app ${currentImg}`}>
-        <h1 className='app__title animated-heading'>
-          Fortune C<span className='animated-heading'><img className='donnutLetter' src={o} alt="" /></span>
-                  <span className='animated-heading'><img className='donnutLetter' src={o} alt=""></img></span>kies</h1>
-        <QuoteBox quote={quote}
-                  handleRandomPhrase={handleRandomPhrase}/>
-      </main>
-  )
+    <main className={`app ${currentImg}`}>
+      <h1 className="app__title animated-heading">
+        Fortune C
+        <span className="animated-heading">
+          <img className="donnutLetter" src={o} alt="" />
+        </span>
+        <span className="animated-heading">
+          <img className="donnutLetter" src={o} alt=""></img>
+        </span>
+        kies
+      </h1>
+      <QuoteBox quote={quote} handleRandomPhrase={handleRandomPhrase} />
+    </main>
+  );
 }
 
-export default App
+export default App;
--- src/components/QuoteBox/QuoteBox.jsx
+++ src/components/QuoteBox/QuoteBox.jsx
@@ -1,19 +1,18 @@
-import './quoteBox.css'
-import paper from '../../utils/paper.svg'
+import "./quoteBox.css";
+import paper from "../../utils/paper.svg";
 
-const QuoteBox = ({ handleRandomPhrase, quote}) => {
-
+const QuoteBox = ({ handleRandomPhrase, quote }) => {
   return (
-  <>
-    <section className="phrase__container">
-      <img className='paper__phrase' src={paper} alt="" />
-      <p className='fortune__phrase'>{quote.phrase}</p>
-    </section>
-    <button className='change__phrase-btn' onClick={handleRandomPhrase}>View other</button>
-    <section className='section__author'>
-      {quote.author}
-    </section>
-  </>
-  )
-}
-export default QuoteBox
+    <>
+      <section className="phrase__container">
+        <img className="paper__phrase" src={paper} alt="" />
+        <p className="fortune__phrase">{quote.phrase}</p>
+      </section>
+      <button className="change__phrase-btn" onClick={handleRandomPhrase}>
+        View other
+      </button>
+      <section className="section__author">{quote.author}</section>
+    </>
+  );
+};
+export default QuoteBox;
